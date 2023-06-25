465486ee0473aa5211217cb0cfa0d871159d0e8a
Warlord-K <warlordk28@gmail.com>
2023-06-25 16:33:05 +0530

    Remove Global Loaded

--- app.py
+++ app.py
@@ -3,23 +3,27 @@ from fastapi.responses import FileResponse
 import uvicorn
 from utils.model import load, generate
 import tempfile
+
 LOADED = False
 app = FastAPI()
+
+
 @app.get("/")
 async def root():
     return {"message": "route working"}
 
+
 @app.get("/generate")
-async def generate_(image_path : str, cloth_path : str = None, prompt : str = None):
+async def generate_(image_path: str, cloth_path: str = None, prompt: str = None):
     """
     Generate Image.
 
-    Request Body 
+    Request Body
     request = {
         "image" : Input Image URL
         "cloth" : Cloth Image URL
         "prompt" : Prompt, In case example image is not provided
-    }    
+    }
 
     Return Body:
     {
@@ -27,13 +31,12 @@ async def generate_(image_path : str, cloth_path : str = None, prompt : str = No
     }
     """
     using_prompt = True if prompt else False
-    if not LOADED:
-        extractor, model, pipe = load(using_prompt)
-        LOADED = True
+    extractor, model, pipe = load(using_prompt)
     gen = generate(image_path, extractor, model, pipe, cloth_path, prompt)
-    temp_file = tempfile.mkstemp(suffix = '.jpg')
+    temp_file = tempfile.mkstemp(suffix=".jpg")
     gen.save(temp_file[-1])
     return FileResponse(temp_file[-1])
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='0.0.0.0')
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
